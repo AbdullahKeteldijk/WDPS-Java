ea5c66e033b64b9d2e85c70c8099a6b358af58a5
khehn <khehn@mail.uni-mannheim.de>
2017-11-23 21:50:15 +0100

    d

--- src/Spark/SparkScript.java
+++ src/Spark/SparkScript.java
@@ -76,6 +76,11 @@ public class SparkScript {
 			ArrayList<CustomWarcRecord> outputList = new ArrayList<CustomWarcRecord>();
 			ArrayList<AnnotatedRecord> output = new ArrayList<AnnotatedRecord>();
 
+			Properties propsSentence = new Properties();
+			propsSentence.put("language", "english");
+			propsSentence.setProperty("annotators", "tokenize, ssplit");
+			StanfordCoreNLP pipelineSentence = new StanfordCoreNLP(propsSentence);
+
 			Properties props = new Properties();
 			props.put("language", "english");
 			props.setProperty("annotators", "tokenize, ssplit, pos, lemma, ner");
@@ -127,9 +132,23 @@ public class SparkScript {
 				ArrayList<Token> tokensList = new ArrayList<Token>();
 				
 				
-				Annotation documentSentencesTokens = new Annotation(record.getContent());
+				Annotation documentSentences = new Annotation(parsedContent);
+				pipelineSentence.annotate(documentSentences);
+				List<CoreMap> coreMapSentences = documentSentences.get(SentencesAnnotation.class);
+				String clearedText = "";
+				
+				for (CoreMap sentence : coreMapSentences) {
+					edu.stanford.nlp.simple.Sentence countTokensSentence = new edu.stanford.nlp.simple.Sentence(
+							sentence);
+//					 IF SENTENCE HAS MORE THAN 100 TOKENS, DO NOT PROCESS IT!
+					if (countTokensSentence.length() > 100) {
+						continue;
+					} 
+					clearedText += (sentence.toString()+"\n");
+				}
+				Annotation documentSentencesTokens = new Annotation(clearedText);
 				pipeline.annotate(documentSentencesTokens);
-				List<CoreMap>coreMapSentences = documentSentencesTokens.get(SentencesAnnotation.class);
+				coreMapSentences = documentSentencesTokens.get(SentencesAnnotation.class);
 				for (CoreMap sentence : coreMapSentences) {
 					List<CoreLabel> tokens = sentence.get(TokensAnnotation.class);
 					for (CoreLabel t : tokens) {
