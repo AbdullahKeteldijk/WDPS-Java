045c4409c9ce7c5024ca144ac40a5c2466271ee8
khehn <khehn@mail.uni-mannheim.de>
2017-11-28 20:21:56 +0100

    adapter sentence length

--- src/Spark/test.java
+++ src/Spark/test.java
@@ -3,43 +3,20 @@ package Spark;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.net.InetAddress;
 import java.net.URL;
-
-import org.elasticsearch.action.search.SearchResponse;
-import org.elasticsearch.action.search.SearchType;
-import org.elasticsearch.client.Client;
-import org.elasticsearch.client.transport.TransportClient;
-import org.elasticsearch.common.settings.Settings;
-import org.elasticsearch.common.transport.InetSocketTransportAddress;
-import org.elasticsearch.index.query.QueryBuilders;
-import org.elasticsearch.search.SearchHit;
 
 public class test {
 
 
 	
 	public static void main(String[] args) throws IOException {
-		Settings settings = Settings.settingsBuilder()
-		        .put("cluster.name", "web-data-processing-systems").build();
-		
-		Client client = TransportClient.builder().settings(settings).build()
-		        .addTransportAddress(new InetSocketTransportAddress(InetAddress.getByName("http://10.149.0.127"), 9200));
+		String searchTerm = "Obama";
+		URL url = new URL("http://10.149.0.127:9200/freebase/label/_search?q="+searchTerm);
 
-		// on shutdown
-		
-		
-		SearchResponse response = client.prepareSearch("freebase")
-		        .setTypes("type1", "type2")
-		        .setSearchType(SearchType.DFS_QUERY_THEN_FETCH)
-		        .setQuery(QueryBuilders.termQuery("multi", "obama"))                 // Query
-		        .setFrom(0).setSize(60).setExplain(true)
-		        .execute()
-		        .actionGet();
-		
-		for (SearchHit sh: response.getHits()){
-			System.out.println(sh.toString());
+		try (BufferedReader reader = new BufferedReader(new InputStreamReader(url.openStream(), "UTF-8"))) {
+		    for (String line; (line = reader.readLine()) != null;) {
+		        System.out.println(line);
+		    }
 		}
-		client.close();
 	}
 }
